0edaa3feea04974979a0c65a63e857aacddff7fe
unknown <159965658@qq.com>
2018-10-09 11:08:56 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <p class="my_font">大傻逼</p> -->
+  <div id="app"> 
     <router-view/>
   </div>
 </template>
@@ -19,7 +17,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
 <style lang="less" scoped>
