d765186be09c71d3d27d7e2bae5f8bec64b25230
unknown <159965658@qq.com>
2018-10-09 18:57:50 +0800

    app.less

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,8 @@ export default {
 };
 </script>
 
-<style>
+<style lang='less'>
+@import "./assets/app.less";
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,11 +30,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   /* margin-top: 60px; */
-}
-</style>
-<style lang="less" scoped>
-.my_font {
-  // font-size: 24px;
 }
 </style>
 
