f54b40fbdd85eb03d2e8995d3b106b23ee30e21a
马瑞娟 <maruijuan@maruijuandeMBP.lan>
2018-10-10 19:25:35 +0800

    vvv

--- src/view/Activation.vue
+++ src/view/Activation.vue
@@ -1,14 +1,75 @@
 <template>
     <div>
-    		<div class="blue-bg">
-	        	<div class="login-logo">
+    		<div class="biaoti">我是标题</div>
+    		 <div class="blue-bg clearfix">
+    		 	<i class="icon icon-jhsb-saoyisao"></i>
+    		 	<div class="inputs">
+	        		<ul>
+	        			<li>
+	        				<input type="text" placeholder="输入激活码">
+	        			</li>
+	        		</ul>
+	        		<p class="jh-tips">请输入激活码，激活码可安购买的软件上获取</p>
+	    			<div class="button-jh"> 
+	    				<a href="#" class="button jh">确认激活</a>
+	    			</div>
+	        		
 	        	</div>
-	        	 
-        </div>
+	        	
+	        	<div class="error-tips">
+	        		您输入的激活码有误，请查正后输入
+	        	</div>
+        	</div>
     </div>
 </template>
 <script>
 export default {};
 </script>
 <style lang="less" scoped>
+.blue-bg{
+	position: relative;
+}
+.error-tips{
+	width: 530px;
+	height: 200px;
+	background: rgba(0,0,0,0.5);
+	position: absolute;
+	left:50% ;
+	top: 50%;
+	margin-left:-275px ;
+	margin-top: -65px;
+	border-radius: 20px;
+	font-size: 30px;
+	color: #fff;
+	text-align: center;
+	line-height:200px ;
+}
+
+.icon-jhsb-saoyisao{
+	width: 60px;
+	height: 60px;
+	background: url(../assets/jhsb-saoyisao.png) no-repeat 100% 100%;
+	display: block;
+	background-size:100% ;
+	float: right;
+	margin-top: 48px;
+	margin-right: 54px;
+	margin-bottom: 168px;
+}
+.inputs{
+	height:402px;
+	padding-top: 275px;
+	padding-bottom: 224px;
+	ul li input{
+		text-align: left;
+		padding-left: 50px;
+	}
+}
+.jh-tips{
+	font-size: 36px;
+	color: #fff;
+	margin-top: 40px;
+	margin-left: 50px;
+	margin-bottom: 125px;
+}
 </style>
--- src/view/Login.vue
+++ src/view/Login.vue
@@ -37,7 +37,19 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-
+.blue-bg {
+    width: 65%;
+    background: -webkit-linear-gradient(left top, #88d7f7, #00a6e7);
+    background: -o-linear-gradient(bottom right, #88d7f7, #00a6e7);
+    background: -moz-linear-gradient(bottom right, #88d7f7, #00a6e7);
+    background: linear-gradient(to bottom right, #88d7f7, #00a6e7);
+    margin: 0 auto;
+    border-radius: 20px;
+    margin-top: 100px;
+    box-shadow: -1px 1px 30px #ababab;
+    padding-bottom: 110px;
+    min-height: 1000px;
+}
 
 .login-logo{
 	margin: 0 auto; 
@@ -49,78 +61,7 @@ export default {
     background-position: 0 60px;
     margin-bottom: 48px;
 }
-input{
-	outline:none;
-}
-.m-t-60{
-	margin-top:60px;
-}
-.m-t-100{
-	margin-top:100px;
-}
-.inputs{
-	width: 68%;
-    margin: 0 auto; 
-    height: 100px;
-    ul{
-    	li{
-    		background: #fff;
-		    height: 102px;
-		    line-height: 102px;
-		    border-radius: 100px;
-		    position:relative;
-		    label{
-		    	color:#1880c3;
-		    	font-size:36px;
-		    	float:left;
-		    	margin-left: 50px;
-		    }
-		    input::-webkit-input-placeholder { 
-	          color: #8fb9cb; 
-	          font-size: 36px; 
-	      	}
-		    input{  
-			    border: none;
-			    background: none;
-   				text-align: center	;
-   				font-size:36px;
-   				color:#1880c3;
-   				width: 100%;
-			    position: absolute;
-			    left: 0;
-			    text-align: center;
-			    background: none;
-			    height: 102px;
-			    line-height: 102px;
-			    text-align: center;
-		    }
-		    .icon-mima{
-		    	background: url(../assets/mima.png);
-		    	width: 60px;
-		    	height: 60px;
-		    	position: absolute;
-			    right: -120px;
-			    background-size: 100%;
-			    top: 21px;
-		    }
-    	}
-    }
-}
-.forget-mima{
-	.forget,.change{
-		font-size: 36px;
-	    color: #fff;
-	    margin-top: 42px;
-	}
-	.forget{
-		float:left;
-		margin-left:50px;
 
-	}
-	.change{
-		float: right;
-	    margin-right: 50px;
-	}
-}
+
 
 </style>
