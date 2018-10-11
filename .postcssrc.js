1766f8dac6bed488ff03ddf363022d9c5eacc968
unknown <159965658@qq.com>
2018-10-11 19:23:16 +0800

    xiugai 适配

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,10 +6,10 @@ module.exports = {
     "postcss-write-svg": { utf8: false },
     "postcss-cssnext": {},
     "postcss-px-to-viewport": {
-      viewportWidth: 2048, // (Number) The width of the viewport.
-      viewportHeight: 1536, // (Number) The height of the viewport. 
+      viewportWidth: 1536, // (Number) The width of the viewport.
+      viewportHeight: 2048, // (Number) The height of the viewport. 
       unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
-      viewportUnit: 'vw', // (String) Expected units. 
+      viewportUnit: 'vh', // (String) Expected units. 
       selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
       minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
       mediaQuery: false // (Boolean) Allow px to be converted in media queries.
--- src/view/SwitchAccount.vue
+++ src/view/SwitchAccount.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        	<div>切换账号</div> 
+        	 <app-header :ltitle='"后退"' :ctitle='"切换登录账号"'></app-header>
         <div class="blue-bg clearfix">
 	         <h3>请选择登录您自已的帐号</h3>
 	         <div class="pic clearfix">
@@ -22,48 +22,47 @@
 export default {};
 </script>
 <style lang="less" scoped>
-.blue-bg{
-	h3{
-		font-size: 48px;
-		color: #fff;
-		text-align: center;
-		padding-top: 190px;
-		margin-bottom: 185px;
-	}
-	.pic{
-		margin: 0 auto;
-		width: 742px;
-		padding-bottom: 190px;
-	}
-	dl{
-		float: left;
-		dt{
-			width: 220px;
-			height: 220px;
-			border-radius: 50%;
-			
-		}
-		dd{
-			text-align: center;
-			font-size: 36px;
-			color: #fff;
-			margin-top: 30px;
-		}
-	}
-	dl.active{
-		dt{
-			background-color: #87c2f9;
-			background: url(../assets/pic-active.png) no-repeat;
-			background-size:100% ;
-		}
-	}
-	dl.gray{
-		dt{
-			background-color: #d0d0d0;
-			background: url(../assets/pic-gray.png) no-repeat;
-			background-size:100% ;
-		}
-	}
+.blue-bg {
+  h3 {
+    font-size: 48px;
+    color: #fff;
+    text-align: center;
+    padding-top: 190px;
+    margin-bottom: 185px;
+  }
+  .pic {
+    margin: 0 auto;
+    width: 742px;
+    padding-bottom: 190px;
+  }
+  dl {
+    float: left;
+    dt {
+      width: 220px;
+      height: 220px;
+      border-radius: 50%;
+    }
+    dd {
+      text-align: center;
+      font-size: 36px;
+      color: #fff;
+      margin-top: 30px;
+    }
+  }
+  dl.active {
+    dt {
+      background-color: #87c2f9;
+      background: url(../assets/pic-active.png) no-repeat;
+      background-size: 100%;
+    }
+  }
+  dl.gray {
+    dt {
+      background-color: #d0d0d0;
+      background: url(../assets/pic-gray.png) no-repeat;
+      background-size: 100%;
+    }
+  }
 }
 </style>
 
