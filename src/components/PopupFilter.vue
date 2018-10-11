f94e6b66e67920299ee3367ab29c59ea678f1982
unknown <159965658@qq.com>
2018-10-11 16:52:01 +0800

    筛选

---
+++ src/components/PopupFilter.vue
@@ -0,0 +1,88 @@
+<template>
+<!--弹出层背景-->
+<div class="popup_bg" v-show="isShow">
+    <transition name="slide-fade" mode="out-in">
+
+        <!--弹出层-筛选-->
+        <div class="popup_filter" v-show="isShow">
+            <h2>筛选</h2>
+            <ul class="forms">
+                <li class="clearfix time">
+                    <label for="">时间</label>
+                    <input type="text" />
+                    <p></p>
+                    <input type="text" />
+        		</li>
+                <li class="clearfix">
+                    <label for="">上传状态</label>
+                    <input type="text" />
+                    <div class="arrow"></div>
+                </li>
+                <li class="clearfix">
+                    <label for="">创建人</label>
+                    <input type="text" />
+        		</li>
+                <li class="clearfix">
+                    <label for="">机构</label>
+                    <input type="text" />
+        		</li>
+                <li>
+                    <button class="btn-save" @click="save">保存</button>
+                </li>
+            </ul>
+        </div>
+    </transition>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    isShow: false
+  },
+  methods: {
+    save() {
+      this.$emit("search");
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.popup_filter {
+  width: 824px;
+  background: #fff;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+
+  input {
+    font-size: 35px;
+  }
+
+  h2 {
+    height: 84px;
+    border-bottom: 1px solid #dcdcdc;
+    text-align: center;
+    font-size: 36px;
+    color: #282828;
+    padding-top: 72px;
+    font-weight: bold;
+  }
+}
+
+.forms {
+  height: calc(100% - 156px);
+  overflow-y: auto;
+
+  li {
+    width: 776px;
+  }
+}
+
+.btn-save {
+  margin-top: 100px;
+  margin-left: 188px;
+}
+</style>
--- src/view/index.vue
+++ src/view/index.vue
@@ -25,7 +25,7 @@
             <div class="center-content ">
                 <div class="total">
                     <p>当前共有数据：<span>10</span></p>
-                    <i class="icon icon-paixu"></i>
+                    <i class="icon icon-paixu" @click="filterVis = !filterVis"></i>
                 </div>
                 	<div class="card_area">
 	                <ul class="card clearfix">
@@ -200,71 +200,30 @@
             		</div>
             	</div>
         </div>
-        <!--弹出层背景-->
-        <!-- <div class="popup_bg"></div> -->
-        <!--弹出层-筛选-->
-        <!-- <div class="popup_filter">
-        		<h2>筛选</h2>
-        		<ul class="forms">
-        			<li class="clearfix time">
-        				<label for="">时间</label>
-        				<input type="text" />
-        				<p></p>
-        				<input type="text" /> 
-        			</li>
-        			<li class="clearfix">
-        				<label for="">上传状态</label>
-        				<input type="text" />
-        				<div class="arrow"></div>
-        			</li>
-        			<li class="clearfix">
-        				<label for="">创建人</label>
-        				<input type="text" />
-        			</li>
-        			<li class="clearfix">
-        				<label for="">机构</label>
-        				<input type="text" />
-        			</li>
-        			<li>
-        				<button class="btn-save">保存</button>
-        			</li>
-        		</ul>
-        </div> -->
+		<popup-filter :isShow = 'filterVis' v-on:search='searchFilter'></popup-filter>
     </div>
     
 </template>
 <script>
-export default {};
+import PopupFilter from "@/components/PopupFilter";
+export default {
+  components: {
+    PopupFilter
+  },
+  data() {
+    return {
+      filterVis: false
+    };
+  },
+  methods: {
+    searchFilter() {
+      console.log("搜索");
+      this.filterVis = false;
+    }
+  }
+};
 </script>
 <style lang="less" scoped>
-.popup_filter {
-  width: 824px;
-  background: #fff;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  right: 0;
-  h2 {
-    height: 84px;
-    border-bottom: 1px solid #dcdcdc;
-    text-align: center;
-    font-size: 36px;
-    color: #282828;
-    padding-top: 72px;
-    font-weight: bold;
-  }
-}
-.forms {
-  height: calc(100% - 156px);
-  overflow-y: auto;
-  li {
-    width: 776px;
-  }
-}
-.btn-save {
-  margin-top: 100px;
-  margin-left: 188px;
-}
 .index-page {
   background: #f3f6fc;
   height: 100%;
