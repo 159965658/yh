321f108da4c961bb6290dd2667683c9f831b1570
unknown <159965658@qq.com>
2018-10-11 16:59:41 +0800

    cwhi

--- src/router/index.js
+++ src/router/index.js
@@ -26,8 +26,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
     }, {
       path: '/login',
       name: 'Login',
